9313211ed266eb25e0639a57deda34e76e6b4d1b
AkobirAbduvahobov <akobirabduvahobov@gmail.com>
2024-01-11 17:06:39 +0500

    done

--- NTierApplication.Service/IItemService.cs
+++ NTierApplication.Service/IItemService.cs
@@ -1,13 +1,12 @@
 ﻿using NTierApplication.Service.ViewModels;
 
-namespace NTierApplication.Service
+namespace NTierApplication.Service;
+
+public interface IItemService
 {
-    public interface IItemService
-    {
-        ItemViewModelExtended CreateNew(ItemViewModelShort item);
-        void Update(ItemViewModelExtended item);
-        void Delete(long itemId);
-        ICollection<ItemViewModelExtended> GetItems();
-        ItemViewModelExtended GetById(long id);
-    }
+    ItemViewModelExtended CreateNew(ItemViewModelShort item);
+    void Update(ItemViewModelExtended item);
+    void Delete(long itemId);
+    ICollection<ItemViewModelExtended> GetItems();
+    ItemViewModelExtended GetById(long id);
 }
